8766ac22dc8b22b39d481f38125c1c445b7ead92
Imię <Imi@DESKTOP-OTD02HG>
2023-11-12 00:06:16 +0100

    Add project files.

---
+++ 01/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string tjena = "Hej";
+            string smile = " :)";
+            string YourName;
+
+            void Name()
+            {
+                Console.WriteLine("What is your name?");
+                YourName = Console.ReadLine();
+            }
+            //      
+            do
+            {
+                Name();
+            }
+            while (string.IsNullOrEmpty(YourName) || string.IsNullOrWhiteSpace(YourName));
+
+
+            void Hello()
+            {
+                Console.WriteLine(tjena + ", hej " + YourName + smile);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Hello();
+
+
+
+
+
+
+            //next exercise
+            /*
+            for (var i = 0; i < 10; i++)
+            {
+                Console.Write(i);
+                if (i < 9)
+                {
+                    Console.Write(" - ");
+                }
+
+                if (i == 9)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("End of this excercise");
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            */
+        }
+
+    }
+
+}
